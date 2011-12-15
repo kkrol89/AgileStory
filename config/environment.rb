4f4b5ca0de61f7cf3fc911b285b6b91e86b41733
Konrad Król <konrad.krol89@gmail.com>
2011-12-15 23:12:11 +0100

    Add smtp configuration for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SMTP_USER'],
+  :password => ENV['SMTP_PASSWORD'],
+  :domain => "cold-leaf-1834.heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the rails application
 Zumba::Application.initialize!
