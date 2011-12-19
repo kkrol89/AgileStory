52770a988ae77811bdc2cfc6249a5a12fdf41539
Konrad Król <konrad.krol89@gmail.com>
2011-12-19 23:38:10 +0100

    Switch database strategy to transaction

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,24 +19,6 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # Database cleaner
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.after(:each) do
-    ActionMailer::Base.deliveries.clear
-    DatabaseCleaner.clean
-  end
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    ActionMailer::Base.deliveries.clear
+    DatabaseCleaner.clean
+  end
+end
